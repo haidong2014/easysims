61d7acc9cb672000befc95f582f8e2497747183c
zhanghaidong <1826962986@qq.com>
2014-06-25 23:01:34 +0800

    update from haidong.

--- application/views/teacher_add_v.php
+++ application/views/teacher_add_v.php
@@ -1,4 +1,4 @@
-﻿<?php 
+﻿<?php
 if(empty($sex)){
   $sex=1;
 }
@@ -16,8 +16,8 @@ if(!empty($teacher_id)){
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <title>教师信息登录</title>
-    <link href="<?php echo SITE_URL;?>/statics/ligerUI/skins/Aqua/css/ligerui-all.css" rel="stylesheet" type="text/css" /> 
-    <link href="<?php echo SITE_URL;?>/statics/ligerUI/skins/Gray/css/all.css" rel="stylesheet" type="text/css" /> 
+    <link href="<?php echo SITE_URL;?>/statics/ligerUI/skins/Aqua/css/ligerui-all.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo SITE_URL;?>/statics/ligerUI/skins/Gray/css/all.css" rel="stylesheet" type="text/css" />
     <script src="<?php echo SITE_URL;?>/statics/jquery/jquery-1.3.2.min.js" type="text/javascript"></script>
     <script src="<?php echo SITE_URL;?>/statics/ligerUI/js/core/base.js" type="text/javascript"></script>
     <script src="<?php echo SITE_URL;?>/statics/ligerUI/js/plugins/ligerForm.js" type="text/javascript"></script>
@@ -28,14 +28,14 @@ if(!empty($teacher_id)){
     <script src="<?php echo SITE_URL;?>/statics/ligerUI/js/plugins/ligerDialog.js" type="text/javascript"></script>
     <script src="<?php echo SITE_URL;?>/statics/ligerUI/js/plugins/ligerRadio.js" type="text/javascript"></script>
     <script src="<?php echo SITE_URL;?>/statics/ligerUI/js/plugins/ligerSpinner.js" type="text/javascript"></script>
-    <script src="<?php echo SITE_URL;?>/statics/ligerUI/js/plugins/ligerTextBox.js" type="text/javascript"></script> 
+    <script src="<?php echo SITE_URL;?>/statics/ligerUI/js/plugins/ligerTextBox.js" type="text/javascript"></script>
     <script src="<?php echo SITE_URL;?>/statics/ligerUI/js/plugins/ligerTip.js" type="text/javascript"></script>
-    <script src="<?php echo SITE_URL;?>/statics/jquery-validation/jquery.validate.min.js" type="text/javascript"></script> 
+    <script src="<?php echo SITE_URL;?>/statics/jquery-validation/jquery.validate.min.js" type="text/javascript"></script>
     <script src="<?php echo SITE_URL;?>/statics/jquery-validation/jquery.metadata.js" type="text/javascript"></script>
     <script src="<?php echo SITE_URL;?>/statics/jquery-validation/messages_cn.js" type="text/javascript"></script>
 
     <script type="text/javascript">
-       /* var eee;
+        var eee;
         $(function ()
         {
             $.metadata.setType("attr", "validate");
@@ -45,7 +45,7 @@ if(!empty($teacher_id)){
                 {
                     if (element.hasClass("l-textarea"))
                     {
-                        element.ligerTip({ content: lable.html(), target: element[0] }); 
+                        element.ligerTip({ content: lable.html(), target: element[0] });
                     }
                     else if (element.hasClass("l-text-field"))
                     {
@@ -64,7 +64,7 @@ if(!empty($teacher_id)){
                 submitHandler: function ()
                 {
                     $("form .l-text,.l-textarea").ligerHideTip();
-                    //$("form").submit();
+                    $("form").submit();
                 }
             });
             $("form").ligerForm();
@@ -72,7 +72,7 @@ if(!empty($teacher_id)){
             {
                 alert(v.element($("#txtName")));
             });
-        });  */
+        });
     </script>
     <style type="text/css">
         body{ font-size:12px;}
@@ -83,49 +83,49 @@ if(!empty($teacher_id)){
     </style>
 </head>
 <body>
-<div id="pageloading"></div>  
-	<a href="<?php echo SITE_URL;?>/teacher_c">教师一览</a>&nbsp>&nbsp
-    <form name="form" method="post" action="<?php echo $action;?>" id="form" action="">
+<div id="pageloading"></div>
+  <a href="<?php echo SITE_URL;?>/teacher_c">教师一览</a>&nbsp>&nbsp
+    <form name="form" method="post" action="<?php echo $action;?>" id="form">
     <div></div>
         <table cellpadding="0" cellspacing="0" class="l-table-edit" >
-		    <tr>
+            <tr>
                 <td align="right" class="l-table-edit-td">教师编号:</td>
-                <td align="left" class="l-table-edit-td"><input name="teacher_no" type="text" id="teacher_no" ltype="text" value="<?php echo @$teacher_no ?>" validate="{required:true,minlength:3,maxlength:10}" /></td>
+                <td align="left" class="l-table-edit-td"><input name="teacher_no" type="text" id="teacher_no" ltype="text" value="<?php echo @$teacher_no ?>" validate="{required:true,maxlength:10}" /></td>
                 <td align="left"></td>
             </tr>
             <tr>
                 <td align="right" class="l-table-edit-td">教师姓名:</td>
-                <td align="left" class="l-table-edit-td"><input name="teacher_name" type="text" id="teacher_name" value="<?php echo @$teacher_name ?>" ltype="text" validate="{required:true,minlength:3,maxlength:10}" /></td>
+                <td align="left" class="l-table-edit-td"><input name="teacher_name" type="text" id="teacher_name" ltype="text" value="<?php echo @$teacher_name ?>" validate="{required:true,maxlength:30}" /></td>
                 <td align="left"></td>
             </tr>
             <tr>
                 <td align="right" class="l-table-edit-td" valign="top">性别:</td>
                 <td align="left" class="l-table-edit-td">
-                    <input id="sex" type="radio" name="sex" value="1" <?php echo (@$sex==1?"checked":"")?> /><label for="rbtnl_0">男</label>
-					<input id="sex" type="radio" name="sex" value="2" <?php echo (@$sex==2?"checked":"")?>/><label for="rbtnl_1">女</label>
+                    <input id="sex_0" type="radio" name="sex" value="1" <?php echo (@$sex==1?"checked":"")?>/><label for="sex_0">男</label>
+                    <input id="sex_1" type="radio" name="sex" value="2" <?php echo (@$sex==2?"checked":"")?>/><label for="sex_1">女</label>
                 </td><td align="left"></td>
             </tr>
             <tr>
                 <td align="right" class="l-table-edit-td">出生日期:</td>
                 <td align="left" class="l-table-edit-td">
-                    <input name="birthday" type="text" id="birthday" ltype="date" value="<?php echo @$birthday ?>"  validate="{required:true}" />
+                    <input name="birthday" type="text" id="birthday" ltype="date" value="<?php echo @$birthday ?>" validate="{required:true}"/>
                 </td><td align="left"></td>
             </tr>
             <tr>
                 <td align="right" class="l-table-edit-td" valign="top">专/兼职:</td>
                 <td align="left" class="l-table-edit-td">
-                    <input id="property" type="radio" name="property" value="1" <?php echo (@$property==1?"checked":"")?>/><label for="rbtnl_0">专</label>
-					<input id="property" type="radio" name="property" value="2" <?php echo (@$property===2?"checked":"")?>/><label for="rbtnl_1">兼</label>
+                    <input id="property_0" type="radio" name="property" value="1" <?php echo (@$property==1?"checked":"")?>/><label for="property_0">专</label>
+                    <input id="property_1" type="radio" name="property" value="2" <?php echo (@$property==2?"checked":"")?>/><label for="property_1">兼</label>
                 </td><td align="left"></td>
             </tr>
-		    <tr>
+        <tr>
                 <td align="right" class="l-table-edit-td">任课科目:</td>
-                <td align="left" class="l-table-edit-td"><input name="course" type="text" id="course" ltype="text"  value="<?php echo @$course ?>"  validate="{required:true,minlength:3,maxlength:10}" /></td>
+                <td align="left" class="l-table-edit-td"><input name="course" type="text" id="course" ltype="text"  value="<?php echo @$course ?>"  validate="{maxlength:20}" /></td>
                 <td align="left"></td>
             </tr>
-			<tr>
+      <tr>
                 <td align="right" class="l-table-edit-td">手机:</td>
-                <td align="left" class="l-table-edit-td"><input name="telephone" type="text" id="telephone" ltype="text"  value="<?php echo @$telephone ?>"   validate="{minlength:11,maxlength:11}" /></td>
+                <td align="left" class="l-table-edit-td"><input name="telephone" type="text" id="telephone" ltype="text"  value="<?php echo @$telephone ?>" validate="{maxlength:12}" /></td>
                 <td align="left"></td>
             </tr>
              <tr>
@@ -136,20 +136,20 @@ if(!empty($teacher_id)){
             <tr>
                 <td align="right" class="l-table-edit-td" valign="top">系统用户:</td>
                 <td align="left" class="l-table-edit-td">
-                     <input id="system_user" type="checkbox" name="system_user" <?php echo (@$system_user===1?"checked":"")?> /><label for="CheckBoxList1_0">有效</label>
+                     <input id="system_user_0" type="checkbox" name="system_user" <?php echo (@$system_user===1?"checked":"")?> /><label for="system_user_0">有效</label>
                 </td><td align="left"></td>
-            </tr>  
+            </tr>
             <tr>
                 <td align="right" class="l-table-edit-td">备注:</td>
-                <td align="left" class="l-table-edit-td"> 
-                <textarea cols="100" rows="4" class="l-textarea" id="remarks" name="remarks"   style="width:400px"><?php echo @$remarks ?></textarea>
+                <td align="left" class="l-table-edit-td">
+                <textarea cols="100" rows="4" class="l-textarea" id="remarks" name="remarks" style="width:400px" validate="{maxlength:100}"><?php echo @$remarks ?></textarea>
                 </td><td align="left"></td>
             </tr>
         </table>
-		<br />
-		<input type="hidden" name="teacher_id"  value="<?php echo @$teacher_id?>" />
-		<input type="submit" value="提交" class="l-button l-button-submit" /> 
-		<input type="reset" value="重置" class="l-button l-button-reset"/>
+    <br />
+    <input type="hidden" name="teacher_id"  value="<?php echo @$teacher_id?>" />
+    <input type="submit" value="提交" class="l-button l-button-submit" />
+    <input type="reset" value="重置" class="l-button l-button-reset"/>
     </form>
     <div style="display:none"></div>
 </body>
--- application/views/teacher_lst_v.php
+++ application/views/teacher_lst_v.php
@@ -2,10 +2,10 @@
 <?php $sex_name=array(1=>'男',2=>'女');?>
 <script type="text/javascript">
 function goreg(){
-	location.href='<?php echo SITE_URL;?>/teacher_c/add_teacher_init';
+  location.href='<?php echo SITE_URL;?>/teacher_c/add_teacher_init';
 }
 function delTeacher(parm){
-	$.ligerDialog.confirm('确定要删除这名老师吗？', function (yes)
+  $.ligerDialog.confirm('确定要删除这名老师吗？', function (yes)
   {
       if(yes){
           location.href = parm;
@@ -16,39 +16,39 @@ function delTeacher(parm){
 <div id="searchbar">
     编号或姓名：<input id="txtKey" type="text" />
     <input id="search" type="button" value=" 查 询 " onclick="search_click()" />
-	<input id="regist" type="button" value=" 教师信息登录 " onclick="goreg()" />
+  <input id="regist" type="button" value=" 教师信息登录 " onclick="goreg()" />
 </div>
-	<br>
-	<table width="800" border="1" cellpadding="0" cellspacing="0" bordercolor="#999999" >
-	  <tr style="background:blue;"  >
-		<th height="20" scope="row" style="text-align:center;vertical-align:middle"><font color=white>教师编号</font></th>
-		<th height="20" scope="row" style="text-align:center;vertical-align:middle"><font color=white>教师姓名</font></th>
-		<th height="20" scope="row" style="text-align:center;vertical-align:middle"><font color=white>性别</font></th>
-		<th height="20" scope="row" style="text-align:center;vertical-align:middle"><font color=white>年龄</font></th>
-		<th height="20" scope="row" style="text-align:center;vertical-align:middle"><font color=white>专/兼职</font></th>
-		<th height="20" scope="row" style="text-align:center;vertical-align:middle"><font color=white>任课科目</font></th>
-		<th height="20" scope="row" style="text-align:center;vertical-align:middle"><font color=white>手机号码</font></th>
-		<th height="20" scope="row" style="text-align:center;vertical-align:middle"><font color=white>电子邮件</font></th>
-		<th height="20" scope="row" style="text-align:center;vertical-align:middle"><font color=white>操作</font></th>
-	  </tr>
-	  <tr>
-	  <?php foreach($teacherData as $key => $value) {?>
-		<td height="20" scope="row"><?php echo $value['teacher_no']?></td>
-		<td height="20" scope="row"><?php echo $value['teacher_name']?></td>
-		<td height="20" scope="row"><?php echo $sex_name[$value['sex']];?></td>
-		<td height="20" scope="row"><?php echo $value['birthday']?></td>
-		<td height="20" scope="row"><?php echo $value['property']?></td>
-		<td height="20" scope="row"><?php echo $value['course']?></td>
-		<td height="20" scope="row"><?php echo $value['telephone']?></td>
-		<td height="20" scope="row"><?php echo $value['email']?></td>
-		<td height="20" scope="row" style="text-align:center;vertical-align:middle">
-		
-		<a href="<?php echo SITE_URL;?>/teacher_c/view_teacher_init/<?php echo $value['teacher_id']?>">查看</a> | 
-		<a href="<?php echo SITE_URL;?>/teacher_c/upd_teacher_init/<?php echo $value['teacher_id']?>">编辑</a> | 
-		<a href="#" onclick="delTeacher('<?php echo SITE_URL;?>/teacher_c/delete_teacher/<?php echo $value['teacher_id']?>')">删除</a>
-		</td>
-	  </tr>
-	  <?php }?>
-	</table>
+  <br>
+  <table width="800" border="1" cellpadding="0" cellspacing="0" bordercolor="#999999" >
+    <tr style="background:blue;"  >
+    <th height="20" scope="row" style="text-align:center;vertical-align:middle"><font color=white>教师编号</font></th>
+    <th height="20" scope="row" style="text-align:center;vertical-align:middle"><font color=white>教师姓名</font></th>
+    <th height="20" scope="row" style="text-align:center;vertical-align:middle"><font color=white>性别</font></th>
+    <th height="20" scope="row" style="text-align:center;vertical-align:middle"><font color=white>年龄</font></th>
+    <th height="20" scope="row" style="text-align:center;vertical-align:middle"><font color=white>专/兼职</font></th>
+    <th height="20" scope="row" style="text-align:center;vertical-align:middle"><font color=white>任课科目</font></th>
+    <th height="20" scope="row" style="text-align:center;vertical-align:middle"><font color=white>手机号码</font></th>
+    <th height="20" scope="row" style="text-align:center;vertical-align:middle"><font color=white>电子邮件</font></th>
+    <th height="20" scope="row" style="text-align:center;vertical-align:middle"><font color=white>操作</font></th>
+    </tr>
+    <tr>
+    <?php foreach($teacherData as $key => $value) {?>
+    <td height="20" scope="row"><?php echo $value['teacher_no']?></td>
+    <td height="20" scope="row"><?php echo $value['teacher_name']?></td>
+    <td height="20" scope="row"><?php echo $sex_name[$value['sex']];?></td>
+    <td height="20" scope="row"><?php echo $value['birthday']?></td>
+    <td height="20" scope="row"><?php echo $value['property']?></td>
+    <td height="20" scope="row"><?php echo $value['course']?></td>
+    <td height="20" scope="row"><?php echo $value['telephone']?></td>
+    <td height="20" scope="row"><?php echo $value['email']?></td>
+    <td height="20" scope="row" style="text-align:center;vertical-align:middle">
+
+    <a href="<?php echo SITE_URL;?>/teacher_c/view_teacher_init/<?php echo $value['teacher_no']?>">查看</a> |
+    <a href="<?php echo SITE_URL;?>/teacher_c/upd_teacher_init/<?php echo $value['teacher_no']?>">编辑</a> |
+    <a href="#" onclick="delTeacher('<?php echo SITE_URL;?>/teacher_c/delete_teacher/<?php echo $value['teacher_no']?>')">删除</a>
+    </td>
+    </tr>
+    <?php }?>
+  </table>
 
 
