a54d4531ed8d8a8f7417b32bed541f3fdd813ace
zhanghaidong <1826962986@qq.com>
2014-07-08 22:31:42 +0800

    update from haidong

---
+++ application/resource/demo/ClassData.js
@@ -0,0 +1,5 @@
+﻿var ClassData = {Rows:
+[
+{"ClassNo":"C2014001","ClassName":"建筑动画班","StartDate":"20140101","EndDate":"20141231","Teacher":"张三","ClassRoom":"F0501","Numbers":"15","Status":"招生","Operation":"查看 | 编辑 | 删除"},
+{"ClassNo":"C2014002","ClassName":"室内设计班","StartDate":"20140101","EndDate":"20141231","Teacher":"李四","ClassRoom":"F0502","Numbers":"20","Status":"开课","Operation":"查看 | 编辑 | 删除"}
+],Total:2};
