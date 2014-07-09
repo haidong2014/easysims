1f6e77d2f93b3a8e2ab74471a0a079843f9753f5
zhanghaidong <1826962986@qq.com>
2014-07-09 22:39:37 +0800

    update from haidong

---
+++ application/resource/demo/RoleData.js
@@ -0,0 +1,6 @@
+﻿var RoleData = 
+{Rows:
+[
+{"FunctionName":"招生信息输入查看"},
+{"FunctionName":"招生信息输入编辑"}
+],Total:2};
---
+++ application/resource/demo/UsergroupsData.js
@@ -0,0 +1,10 @@
+﻿var UsergroupsData = 
+{Rows:
+[
+{"UserGroupNo":"GU01","UserGroupName":"系统管理员","Operation":"查看 | 编辑 | 删除"},
+{"UserGroupNo":"GU02","UserGroupName":"校长","Operation":"查看 | 编辑 | 删除"},
+{"UserGroupNo":"GU03","UserGroupName":"教学总监","Operation":"查看 | 编辑 | 删除"},
+{"UserGroupNo":"GU04","UserGroupName":"班主任","Operation":"查看 | 编辑 | 删除"},
+{"UserGroupNo":"GU05","UserGroupName":"任课教师","Operation":"查看 | 编辑 | 删除"},
+{"UserGroupNo":"GU06","UserGroupName":"学生","Operation":"查看 | 编辑 | 删除"}
+],Total:6};
