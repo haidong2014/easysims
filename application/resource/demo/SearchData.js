64217a0ac329a69e29f1f7e76ba8e65bb1ecd946
zhanghaidong <1826962986@qq.com>
2014-07-09 22:53:46 +0800

    update by haidong

---
+++ application/resource/demo/SearchData.js
@@ -0,0 +1,6 @@
+﻿var SearchData = 
+{Rows:
+[
+{"CourseName":"建筑动画课程信息","ClassName":"2014建筑动画课程信息班","Teacher":"张三","Subject":"基础","SjtTeacher":"李四","Student":"王小宝","Works":"未来幻想","WorksEvaluation":"90"},
+{"CourseName":"建筑动画课程信息","ClassName":"2014建筑动画课程信息班","Teacher":"张三","Subject":"基础","SjtTeacher":"李四","Student":"王大宝","Works":"科技之城","WorksEvaluation":"98"}
+],Total:2};
