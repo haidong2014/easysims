bf362d16bd155ac8b3c9d87793b1d6ddcdd9f2a4
zhanghaidong <1826962986@qq.com>
2014-07-10 22:21:06 +0800

    update from haidong

--- application/resource/demo/SearchData.js
+++ application/resource/demo/SearchData.js
@@ -1,6 +1,6 @@
 ﻿var SearchData = 
 {Rows:
 [
-{"CourseName":"建筑动画课程信息","ClassName":"2014建筑动画课程信息班","Teacher":"张三","Subject":"基础","SjtTeacher":"李四","Student":"王小宝","Works":"未来幻想","WorksEvaluation":"90"},
-{"CourseName":"建筑动画课程信息","ClassName":"2014建筑动画课程信息班","Teacher":"张三","Subject":"基础","SjtTeacher":"李四","Student":"王大宝","Works":"科技之城","WorksEvaluation":"98"}
+{"CourseName":"建筑动画课程信息","ClassName":"2014建筑动画课程信息班","Teacher":"张三","Student":"王小宝","Sex":"男","Age":"21","AncestralHome":"辽宁省铁岭市铁法县大明镇","GraduateSchool":"大连理工大学","Graduate":"本科","Specialty":"机械设计","WorksEvaluation":"90","AttendanceEvaluation":"90","StartDate":"2014-01-01","EndDate":"2014-12-31","FollowCity":"大连","FollowSalary":"12000","FollowPosition":"设计师"},
+{"CourseName":"建筑动画课程信息","ClassName":"2014建筑动画课程信息班","Teacher":"张三","Student":"王大宝","Sex":"男","Age":"22","AncestralHome":"吉林省长春市","GraduateSchool":"大连铁道学院","Graduate":"大专","Specialty":"计算机科学与技术","WorksEvaluation":"98","AttendanceEvaluation":"95","StartDate":"2014-01-01","EndDate":"2014-12-31","FollowCity":"北京","FollowSalary":"20000","FollowPosition":"设计师"}
 ],Total:2};
