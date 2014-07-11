4f3b7d637a20ffba71882f7314a21573af0c8f6a
zhanghaidong <1826962986@qq.com>
2014-07-11 23:54:46 +0800

    update by haidong

---
+++ application/resource/demo/WorkslstData.js
@@ -0,0 +1,5 @@
+﻿var WorkslstData = {Rows:
+[
+{"WorksNo":"<a href='works_add.html'>NO00001<a>","WorksName":"未来世界","WorksStudent":"王小宝","UpTimes":"2014-07-01","WorksScore":"90","WorksReview":"<a href='works_grade.html'>评分</a>"},
+{"WorksNo":"<a href='works_add.html'>NO00002<a>","WorksName":"科技幻想","WorksStudent":"王大宝","UpTimes":"2014-07-01","WorksScore":"98","WorksReview":"<a href='works_grade.html'>评分</a>"},
+],Total:2};
