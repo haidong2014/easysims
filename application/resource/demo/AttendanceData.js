8eb19d08f78d754bf889ae72ef2b751cb95f0773
zhanghaidong <1826962986@qq.com>
2014-07-11 21:18:07 +0800

    update by haidong

--- application/resource/demo/AttendanceData.js
+++ application/resource/demo/AttendanceData.js
@@ -1,5 +1,5 @@
 ﻿var AttendanceData = {Rows:
 [
-{"ClassNo":"<a href='attendance_lst.html'>C2014001<a>","ClassName":"建筑动画班","StartDate":"20140101","EndDate":"20141231","Teacher":"张三","ClassRoom":"F0501","Numbers":"15","Status":"招生"},
+{"ClassNo":"<a href='attendance_lst.html'>C2014001<a>","ClassName":"建筑动画班","StartDate":"20140101","EndDate":"20141231","Teacher":"张三","ClassRoom":"F0501","Numbers":"15","Status":"开课"},
 {"ClassNo":"<a href='attendance_lst.html'>C2014002<a>","ClassName":"室内设计班","StartDate":"20140101","EndDate":"20141231","Teacher":"李四","ClassRoom":"F0502","Numbers":"20","Status":"开课"}
 ],Total:2};
