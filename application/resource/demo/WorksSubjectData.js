41abb12dd740d2a984028626ac1b4f27cadf46f2
zhanghaidong <1826962986@qq.com>
2014-07-11 22:55:14 +0800

    update by haidong

---
+++ application/resource/demo/WorksSubjectData.js
@@ -0,0 +1,15 @@
+﻿var WorksSubjectData = {Rows:
+[
+{"SubjectNo":"<a href='works_lst.html'>S000101<a>","SubjectName":"基础","Period":"3周","StartDate":"2014-07-01","EndDate":"2014-12-01","Teacher":"张三","PWorks":"12","AWorks":"12","WorksScore":"98"},
+{"SubjectNo":"<a href='works_lst.html'>S000102<a>","SubjectName":"模型（2阶段）","Period":"3周","StartDate":"2014-07-01","EndDate":"2014-12-01","Teacher":"张三","PWorks":"12","AWorks":"12","WorksScore":"98"},
+{"SubjectNo":"<a href='works_lst.html'>S000103<a>","SubjectName":"室内（2阶段）","Period":"3周","StartDate":"2014-07-01","EndDate":"2014-12-01","Teacher":"张三","PWorks":"12","AWorks":"12","WorksScore":"98"},
+{"SubjectNo":"<a href='works_lst.html'>S000104<a>","SubjectName":"室外（2阶段）","Period":"3周","StartDate":"2014-07-01","EndDate":"2014-12-01","Teacher":"张三","PWorks":"12","AWorks":"12","WorksScore":"98"},
+{"SubjectNo":"<a href='works_lst.html'>S000105<a>","SubjectName":"后期（2阶段）","Period":"3周","StartDate":"2014-07-01","EndDate":"2014-12-01","Teacher":"张三","PWorks":"12","AWorks":"12","WorksScore":"98"},
+{"SubjectNo":"<a href='works_lst.html'>S000106<a>","SubjectName":"动画及项目","Period":"4周","StartDate":"2014-07-01","EndDate":"2014-12-01","Teacher":"张三","PWorks":"12","AWorks":"12","WorksScore":"98"},
+{"SubjectNo":"<a href='works_lst.html'>S000107<a>","SubjectName":"模型高级","Period":"2周","StartDate":"2014-07-01","EndDate":"2014-12-01","Teacher":"张三","PWorks":"12","AWorks":"12","WorksScore":"98"},
+{"SubjectNo":"<a href='works_lst.html'>S000108<a>","SubjectName":"室内高级","Period":"2周","StartDate":"2014-07-01","EndDate":"2014-12-01","Teacher":"张三","PWorks":"12","AWorks":"12","WorksScore":"98"},
+{"SubjectNo":"<a href='works_lst.html'>S000109<a>","SubjectName":"渲染高级","Period":"2周","StartDate":"2014-07-01","EndDate":"2014-12-01","Teacher":"张三","PWorks":"12","AWorks":"12","WorksScore":"98"},
+{"SubjectNo":"<a href='works_lst.html'>S000110<a>","SubjectName":"动画（3阶段）","Period":"2周","StartDate":"2014-07-01","EndDate":"2014-12-01","Teacher":"张三","PWorks":"12","AWorks":"12","WorksScore":"98"},
+{"SubjectNo":"<a href='works_lst.html'>S000111<a>","SubjectName":"后期（3阶段）","Period":"2周","StartDate":"2014-07-01","EndDate":"2014-12-01","Teacher":"张三","PWorks":"12","AWorks":"12","WorksScore":"98"},
+{"SubjectNo":"<a href='works_lst.html'>S000112<a>","SubjectName":"实训","Period":"8周","StartDate":"2014-07-01","EndDate":"2014-12-01","Teacher":"张三","PWorks":"12","AWorks":"12","WorksScore":"98"}
+],Total:12};
