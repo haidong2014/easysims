bca08fb3d0a7ed2068f71501946d5be350785e6c
zhanghaidong <1826962986@qq.com>
2014-07-13 20:46:12 +0800

    update from haidong

--- application/resource/demo/UserData.js
+++ application/resource/demo/UserData.js
@@ -1,6 +1,6 @@
 ﻿var UserData = 
 {Rows:
 [
-{"UserNo":"T0001","UserName":"张三","UserGroup":"教师","Operation":"查看 | 编辑 | 删除"},
-{"UserNo":"T0002","UserName":"李四","UserGroup":"学生","Operation":"查看 | 编辑 | 删除"}
+{"UserNo":"T0001","UserName":"张三","UserGroup":"教师","Status":"有效","Operation":"查看 | 编辑 | 删除"},
+{"UserNo":"T0002","UserName":"李四","UserGroup":"学生","Status":"无效","Operation":"查看 | 编辑 | 删除"}
 ],Total:6};
