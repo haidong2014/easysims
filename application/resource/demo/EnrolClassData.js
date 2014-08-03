2be5e4fc715af1f354f6663e8af6f3d70ea30202
zhanghaidong <1826962986@qq.com>
2014-08-03 13:33:17 +0800

    update from haidong

--- application/resource/demo/EnrolClassData.js
+++ application/resource/demo/EnrolClassData.js
@@ -1,5 +1,5 @@
 ﻿var EnrolClassData = {Rows:
 [
-{"ClassNo":"<a href='enrol_students_lst.html'>C201401<a>","ClassName":"建筑动画班","StartDate":"20140101","EndDate":"20141231","Teacher":"张三","ClassRoom":"F0501","Numbers":"15","Status":"招生"},
-{"ClassNo":"<a href='enrol_students_lst.html'>C201402<a>","ClassName":"室内设计班","StartDate":"20140101","EndDate":"20141231","Teacher":"李四","ClassRoom":"F0502","Numbers":"20","Status":"开课"}
+{"ClassNo":"<a href='enrol_students_lst.html'>C201401<a>","ClassName":"建筑动画长期班","CourseName":"建筑动画课程信息","StartDate":"2014-01-01","EndDate":"2014-12-31","Teacher":"张三","ClassRoom":"F0501","Numbers":"15","Schooling":"15000","Status":"招生","Remark":"备考信息"},
+{"ClassNo":"<a href='enrol_students_lst.html'>C201402<a>","ClassName":"室内设计短期班","CourseName":"室内设计课程信息","StartDate":"2014-01-01","EndDate":"2014-12-31","Teacher":"李四","ClassRoom":"F0502","Numbers":"20","Schooling":"12000","Status":"开课","Remark":"备考信息"}
 ],Total:2};
--- application/resource/demo/WorksData.js
+++ application/resource/demo/WorksData.js
@@ -1,5 +1,5 @@
 ﻿var WorksData = {Rows:
 [
-{"ClassNo":"<a href='works_subject.html'>C2014001<a>","ClassName":"建筑动画班","StartDate":"20140101","EndDate":"20141231","Teacher":"张三","ClassRoom":"F0501","Numbers":"15","Status":"开课"},
-{"ClassNo":"<a href='works_subject.html'>C2014002<a>","ClassName":"室内设计班","StartDate":"20140101","EndDate":"20141231","Teacher":"李四","ClassRoom":"F0502","Numbers":"20","Status":"开课"}
+{"ClassNo":"<a href='works_subject.html'>C2014001<a>","ClassName":"建筑动画班","StartDate":"2014-01-01","EndDate":"2014-12-31","Teacher":"张三","ClassRoom":"F0501","Numbers":"15","Status":"开课"},
+{"ClassNo":"<a href='works_subject.html'>C2014002<a>","ClassName":"室内设计班","StartDate":"2014-01-01","EndDate":"2014-12-31","Teacher":"李四","ClassRoom":"F0502","Numbers":"20","Status":"开课"}
 ],Total:2};
