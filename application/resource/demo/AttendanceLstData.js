28164d21d6052857843aee5e6f71a19fce8ffafe
zhanghaidong <1826962986@qq.com>
2014-08-03 09:48:46 +0800

    update from haidong

--- application/resource/demo/AttendanceLstData.js
+++ application/resource/demo/AttendanceLstData.js
@@ -1,5 +1,5 @@
 ﻿var AttendanceLstData = {Rows:
 [
-{"StudentNo":"NO00001","StudentName":"张三","Telephone":"13800000000","AttendanceStatus_1":"10","AttendanceStatus_2":"2","AttendanceStatus_3":"3","AttendanceStatus_4":"4"},
-{"StudentNo":"NO00002","StudentName":"李四","Telephone":"13800000000","AttendanceStatus_1":"10","AttendanceStatus_2":"2","AttendanceStatus_3":"3","AttendanceStatus_4":"4"}
+{"StudentNo":"<a href='attendance_student_lst.html'>NO00001<a>","StudentName":"张三","Telephone":"13800000000","AttendanceStatus_1":"10","AttendanceStatus_2":"2","AttendanceStatus_3":"3","AttendanceStatus_4":"4"},
+{"StudentNo":"<a href='attendance_student_lst.html'>NO00002<a>","StudentName":"李四","Telephone":"13800000000","AttendanceStatus_1":"10","AttendanceStatus_2":"2","AttendanceStatus_3":"3","AttendanceStatus_4":"4"}
 ],Total:2};
---
+++ application/resource/demo/EvaluationStudentData.js
@@ -0,0 +1,4 @@
+﻿var EvaluationStudentData = {Rows:
+[
+{"StudentNo":"NO00001","Student":"张三","A_Score":"90","B_Score":"90","C_Score":"90","D_Score":"90","Score":"90","Evaluation":"<a href='evaluation_student.html'>评价<a>"}
+],Total:1};
