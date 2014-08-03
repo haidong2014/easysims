181e753efa1adbc4b1d17be455c28c5248e549c8
zhanghaidong <1826962986@qq.com>
2014-08-03 13:47:04 +0800

    update from haidong

--- application/resource/demo/SearchData.js
+++ application/resource/demo/SearchData.js
@@ -1,6 +1,6 @@
 ﻿var SearchData = 
 {Rows:
 [
-{"CourseName":"建筑动画课程信息","ClassName":"2014建筑动画课程信息班","Teacher":"张三","Student":"王小宝","Sex":"男","Age":"21","AncestralHome":"辽宁省铁岭市铁法县大明镇","GraduateSchool":"大连理工大学","Graduate":"本科","Specialty":"机械设计","StartDate":"2014-01-01","EndDate":"2014-12-31","Score":"90","FollowCity":"大连","FollowCompany":"广告设计公式","FollowPosition":"设计师","FollowSalary":"12000"},
-{"CourseName":"建筑动画课程信息","ClassName":"2014建筑动画课程信息班","Teacher":"张三","Student":"王大宝","Sex":"男","Age":"22","AncestralHome":"吉林省长春市","GraduateSchool":"大连铁道学院","Graduate":"大专","Specialty":"计算机科学与技术","StartDate":"2014-01-01","EndDate":"2014-12-31","Score":"98","FollowCity":"北京","FollowCompany":"装潢设计公式","FollowPosition":"设计师","FollowSalary":"20000"}
+{"ClassName":"2014建筑动画课程信息班","CourseName":"建筑动画课程信息","Teacher":"张三","Student":"王小宝","Sex":"男","Age":"21","AncestralHome":"辽宁省铁岭市铁法县大明镇","GraduateSchool":"大连理工大学","Graduate":"本科","Specialty":"机械设计","StartDate":"2014-01-01","EndDate":"2014-12-31","Score":"90","FollowCity":"大连","FollowCompany":"广告设计公式","FollowPosition":"设计师","FollowSalary":"12000"},
+{"ClassName":"2014建筑动画课程信息班","CourseName":"建筑动画课程信息","Teacher":"张三","Student":"王大宝","Sex":"男","Age":"22","AncestralHome":"吉林省长春市","GraduateSchool":"大连铁道学院","Graduate":"大专","Specialty":"计算机科学与技术","StartDate":"2014-01-01","EndDate":"2014-12-31","Score":"98","FollowCity":"北京","FollowCompany":"装潢设计公式","FollowPosition":"设计师","FollowSalary":"20000"}
 ],Total:2};
