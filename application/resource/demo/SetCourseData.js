163374637b06398ecad382a21f5273a6d9c6a6a2
zhanghaidong <1826962986@qq.com>
2014-08-04 19:50:19 +0800

    update from haidong

--- application/resource/demo/SetCourseData.js
+++ application/resource/demo/SetCourseData.js
@@ -1,5 +1,5 @@
 ﻿var SetCourseData = 
 {Rows:
 [
-{"SubjectNo":"S000101","SubjectName":"基础","Period":"3周","StartDate":"<input name='startDate' type='text' id='startDate' />","EndDate":"<input name='endDate' type='text' id='endDate' />","Teacher":"<select name='ddlTeacher12' id='ddlTeacher12' ltype='select'><option value='1'>张三</option><option value='2'>李四</option><option value='3'>王二麻子</option><option value='4'>小淘气</option></select>"}
+{"SubjectNo":"S000101","SubjectName":"基础","Period":"3周","StartDate":"<input name='startDate' type='text' id='startDate' style='width:100px' />","EndDate":"<input name='endDate' type='text' id='endDate' style='width:100px'/>","Teacher":"<select name='ddlTeacher12' id='ddlTeacher12' ltype='select'><option value='1'>张三</option><option value='2'>李四</option><option value='3'>王二麻子</option><option value='4'>小淘气</option></select>"}
 ],Total:1};
--- application/resource/demo/UsergroupsData.js
+++ application/resource/demo/UsergroupsData.js
@@ -1,10 +1,12 @@
 ﻿var UsergroupsData = 
 {Rows:
 [
-{"UserGroupNo":"GU01","UserGroupName":"系统管理员","Operation":"查看 | 编辑 | 删除"},
-{"UserGroupNo":"GU02","UserGroupName":"校长","Operation":"查看 | 编辑 | 删除"},
-{"UserGroupNo":"GU03","UserGroupName":"教学总监","Operation":"查看 | 编辑 | 删除"},
-{"UserGroupNo":"GU04","UserGroupName":"班主任","Operation":"查看 | 编辑 | 删除"},
-{"UserGroupNo":"GU05","UserGroupName":"任课教师","Operation":"查看 | 编辑 | 删除"},
-{"UserGroupNo":"GU06","UserGroupName":"学生","Operation":"查看 | 编辑 | 删除"}
+{"UserGroupNo":"G001","UserGroupName":"系统管理员","Operation":"查看 | 编辑 | 删除"},
+{"UserGroupNo":"G002","UserGroupName":"校长","Operation":"查看 | 编辑 | 删除"},
+{"UserGroupNo":"G003","UserGroupName":"招生总监","Operation":"查看 | 编辑 | 删除"},
+{"UserGroupNo":"G004","UserGroupName":"教学总监","Operation":"查看 | 编辑 | 删除"},
+{"UserGroupNo":"G005","UserGroupName":"就业总监","Operation":"查看 | 编辑 | 删除"},
+{"UserGroupNo":"G006","UserGroupName":"班主任","Operation":"查看 | 编辑 | 删除"},
+{"UserGroupNo":"G007","UserGroupName":"教师","Operation":"查看 | 编辑 | 删除"},
+{"UserGroupNo":"G008","UserGroupName":"学生","Operation":"查看 | 编辑 | 删除"}
 ],Total:6};
