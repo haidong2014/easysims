35f78c50b38cd6d33dd7485e5709e1e7b555520c
zhanghaidong <1826962986@qq.com>
2014-08-05 21:00:57 +0800

    update by haidong

--- application/controllers/menu_c.php
+++ application/controllers/menu_c.php
@@ -1,15 +1,16 @@
-<?php 
+<?php
 class Menu_c extends MY_Controller {
-	public function __construct()
-	{
-		parent::__construct();
-	}
+  public function __construct()
+  {
+    parent::__construct();
+  }
 
-	public function index()
-	{
-		$data = array();
-		$user = $this->session->all_userdata();
-		log_message('info', "####user".var_export($user,true));
-		$this->load->view('menu_v',$data);
-	}
+  public function index()
+  {
+    $data = array();
+    $username = $this->session->userdata('user_name');
+    log_message('info', "####username:".$username);
+    $data["username"] = $username;
+    $this->load->view('menu_v',$data);
+  }
 }
