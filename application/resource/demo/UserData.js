6721bf783169d1cfd986209047cc46bb261365a1
zhanghaidong <1826962986@qq.com>
2014-08-07 17:53:31 +0800

    update by haidong

--- application/resource/demo/UserData.js
+++ application/resource/demo/UserData.js
@@ -1,6 +1,6 @@
 ﻿var UserData = 
 {Rows:
 [
-{"UserNo":"T0001","UserName":"张三","UserGroup":"教师","Status":"有效","Operation":"查看 | 编辑 | 删除"},
-{"UserNo":"T0002","UserName":"李四","UserGroup":"学生","Status":"无效","Operation":"查看 | 编辑 | 删除"}
+{"UserNo":"1000","UserId":"admin","UserName":"张三","UserGroup":"教师","Status":"有效","Operation":"查看 | 编辑 | 删除"},
+{"UserNo":"1001","UserId":"S0001","UserName":"李四","UserGroup":"学生","Status":"无效","Operation":"查看 | 编辑 | 删除"}
 ],Total:6};
