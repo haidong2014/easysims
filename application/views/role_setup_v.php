bc44db26efab16edee88d973881d7f9625b4f144
zhanghaidong <1826962986@qq.com>
2014-08-09 14:04:09 +0800

    update by haidong

--- application/views/role_setup_v.php
+++ application/views/role_setup_v.php
@@ -1,39 +1,14 @@
 ﻿<?php require_once("_header.php");?>
+
 <script type="text/javascript">
-    var rolesetupData = <?php echo $rolesetupData?>;
-    var grid = null;
-    $(function () {
-        grid = $("#maingrid").ligerGrid({
-            checkbox: true,
-            columns: [
-            { display: '功能名称', name: 'function_name', align: 'left', width: 300 }
-            ],  
-            pageSize:10,
-            where : f_getWhere(),
-            data: $.extend(true,{},rolesetupData), 
-            width: '100%',height:'90%'
-        });
+    $(function (){
 
         $("#pageloading").hide();
     });
-    function f_search()
-    {
-        grid.options.data = $.extend(true, {}, CustomersData);
-        grid.loadData(f_getWhere());
-    }
-    function f_getWhere()
-    {
-        if (!grid) return null;
-        var clause = function (rowdata, rowindex)
-        {
-            var key = $("#txtKey").val();
-            return rowdata.CustomerID.indexOf(key) > -1;
-        };
-        return clause; 
-    }
     function search_click()
     {
-        document.form.submit();
+        user_id = document.form.ddlUser.value;
+        location.href='<?php echo SITE_URL;?>/rolesetup_c/search_role/'+user_id;
     }
 </script>
 <style type="text/css">
@@ -46,12 +21,12 @@
 </head>
 
 <body style="padding:10px">
-<div id="pageloading"></div>  
+<div id="pageloading"></div>
 <form name="form" method="post" action="<?php echo SITE_URL.'/rolesetup_c/upd_role';?>" id="form">
     <table cellpadding="0" cellspacing="0" class="l-table-edit" >
         <tr>
             <td align="right" class="l-table-edit-td">用户角色:</td>
-            <td align="left" class="l-table-edit-td">               
+            <td align="left" class="l-table-edit-td">
                 <select name="ddlUser" id="ddlUser" onchange="search_click()">
                     <?php foreach($usergroups as $y){?>
                         <option value="<?php echo $y['id']?>" <?php echo $y['sel']?>><?php echo $y['name']?></option>
@@ -62,9 +37,25 @@
         </tr>
     </table>
     <br />
-    <div id="maingrid" style="margin:0; padding:0"></div>
+
+    <table cellpadding="0" cellspacing="0" class="l-table-edit" >
+        <tr>
+            <td align="right" class="l-table-edit-td">用户权限:</td>
+            <td align="left" class="l-table-edit-td">
+                <?php foreach($functionlist as $y){?>
+                    <?php if($y['role_id']==null) {?>
+                        <input id="<?php echo $y['function_id']?>" type="checkbox" name="<?php echo $y['function_id']?>"/>
+                    <?php } else {?>
+                      <input id="<?php echo $y['function_id']?>" type="checkbox" name="<?php echo $y['function_id']?>" checked/>
+                    <?php } ?>
+                    <label>&nbsp;<?php echo $y['function_name']?></label>
+                <?php } ?>
+            </td>
+            <td align="left"></td>
+        </tr>
+    </table>
     <br />
-    <input type="submit" value="提交" class="l-button l-button-submit" /> 
+    <input type="submit" value="提交" class="l-button l-button-submit" />
 </form>
 <div style="display:none"></div>
 </body>
