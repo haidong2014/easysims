2ba9a0522988c2ef034f3dc846056273e81f282e
zhanghaidong <1826962986@qq.com>
2014-08-15 12:59:24 +0800

    update by haidong

--- application/controllers/attendance_c.php
+++ application/controllers/attendance_c.php
@@ -164,13 +164,35 @@ class Attendance_c extends MY_Controller {
         $i = 0;
         foreach($studentData as $temp){
             $datatmp['student_no'] = $temp['student_no'];
-            $datatmp['am_status'] = $this->input->post('am_'.$i);
-            $datatmp['pm_status'] = $this->input->post('pm_'.$i);
-            $datatmp['status'] = $this->input->post('pm_'.$i);
+            $am_status = $this->input->post('am_'.$i);
+            $pm_status = $this->input->post('pm_'.$i);
+            $datatmp['am_status'] = $am_status;
+            $datatmp['pm_status'] = $pm_status;
+            if ($am_status == "1"){
+                if ($pm_status == "1"){
+                    $datatmp['status'] = "1";
+                } else if ($pm_status == "2") {
+                    $datatmp['status'] = "2";
+                } else if ($pm_status == "3") {
+                    $datatmp['status'] = "3";
+                } else if ($pm_status == "4") {
+                    $datatmp['status'] = "4";
+                } else {
+                    $datatmp['status'] = "";
+                }
+            } else if ($am_status == "2") {
+                $datatmp['status'] = "2";
+            } else if ($am_status == "3") {
+                $datatmp['status'] = "3";
+            } else if ($am_status == "4") {
+                $datatmp['status'] = "4";
+            } else {
+                $datatmp['status'] = "";
+            }
+
             $this->attendance_m->insertAttendance($datatmp);
             $i = $i + 1;
         }
-
 
         $studentData = $this->attendance_m->getAttendanceSumList($data);
         foreach($studentData as &$temp){
@@ -180,19 +202,99 @@ class Attendance_c extends MY_Controller {
         $this->load->view('attendance_lst_v',$data);
     }
 
-     public function show_student($class_no,$student_no){
+     public function show_student($class_no=null,$student_no=null){
         $data = array();
         $studentData = array();
+        $studentName = array();
 
         $user = $this->session->all_userdata();
         log_message('info', "attendance_c show_student user:".var_export($user,true));
         log_message('info', "attendance_c show_student post:".var_export($_POST,true));
 
-        $data['class_no'] = $class_no;
-        $data['student_no'] = $student_no;
+        if (empty($class_no)) {
+            $data['class_no'] = $this->input->post('txtClassNo');
+        } else {
+            $data['class_no'] = $class_no;
+        }
+        if (empty($student_no)) {
+            $data['student_no'] = $this->input->post('txtStudentNo');
+        } else {
+            $data['student_no'] = $student_no;
+        }
+
+        $year = array();
+        $year[0] = array("id"=>"0","name"=>"2014","sel"=>"");
+        $year[1] = array("id"=>"1","name"=>"2015","sel"=>"");
+        $year[2] = array("id"=>"2","name"=>"2016","sel"=>"");
+        $year[3] = array("id"=>"3","name"=>"2017","sel"=>"");
+        $year[4] = array("id"=>"4","name"=>"2018","sel"=>"");
+        $year[5] = array("id"=>"5","name"=>"2019","sel"=>"");
+        $year[6] = array("id"=>"6","name"=>"2020","sel"=>"");
+        $year[7] = array("id"=>"7","name"=>"2021","sel"=>"");
+        $year[8] = array("id"=>"8","name"=>"2022","sel"=>"");
+        $year[9] = array("id"=>"9","name"=>"2023","sel"=>"");
+        $year[10] = array("id"=>"10","name"=>"2024","sel"=>"");
+        $year[11] = array("id"=>"11","name"=>"2025","sel"=>"");
+
+        $month = array();
+        $month[0] = array("id"=>"0","name"=>"01","sel"=>"");
+        $month[1] = array("id"=>"1","name"=>"02","sel"=>"");
+        $month[2] = array("id"=>"2","name"=>"03","sel"=>"");
+        $month[3] = array("id"=>"3","name"=>"04","sel"=>"");
+        $month[4] = array("id"=>"4","name"=>"05","sel"=>"");
+        $month[5] = array("id"=>"5","name"=>"06","sel"=>"");
+        $month[6] = array("id"=>"6","name"=>"07","sel"=>"");
+        $month[7] = array("id"=>"7","name"=>"08","sel"=>"");
+        $month[8] = array("id"=>"8","name"=>"09","sel"=>"");
+        $month[9] = array("id"=>"9","name"=>"10","sel"=>"");
+        $month[10] = array("id"=>"10","name"=>"11","sel"=>"");
+        $month[11] = array("id"=>"11","name"=>"12","sel"=>"");
+
+        $ddlYear = $this->input->post('ddlYear');
+        $ddlMonth = $this->input->post('ddlMonth');
+        $tmpYear = substr(date("Y-m-d"),0,4);
+        $tmpMonth = substr(date("Y-m-d"),5,2);
+        if ($ddlYear=="" || $ddlMonth=="") {
+            $search_ym = $tmpYear.$tmpMonth;
+            $i = 0;
+            foreach($year as $y){
+                if ($y['name']==$tmpYear) {
+                    $year[$i]['sel'] = "selected";
+                    break;
+                }
+                $i = $i + 1;
+            }
+            $i = 0;
+            foreach($month as $m){
+                if ($m['name']==$tmpMonth) {
+                    $month[$i]['sel'] = "selected";
+                    break;
+                }
+                $i = $i + 1;
+            }
+        } else {
+            $search_ymd_start = $year[$ddlYear]['name']."-".$month[$ddlMonth]['name']."-01";
+            $search_ymd_end = $year[$ddlYear]['name']."-".$month[$ddlMonth]['name']."-31";
+            $year[$ddlYear]['sel'] = "selected";
+            $month[$ddlMonth]['sel'] = "selected";
+
+            $data['search_ymd_start'] =  $search_ymd_start;
+            $data['search_ymd_end'] =  $search_ymd_end;
+        }
+
+        $data['year'] = $year;
+        $data['month'] = $month;
+
         $studentData = $this->attendance_m->getAttendanceForStudent($data);
+        $studentName = $this->student_m->getStudentName($data['student_no']);
+        if (!empty($studentName)) {
+            $data['student_name'] = $studentName[0]['student_name'];
+        } else {
+            $data['student_name'] = "";
+        }
 
         $data['studentData'] = @json_encode(array('Rows'=>$studentData));
+
         $this->load->view('attendance_student_lst_v',$data);
      }
 }
