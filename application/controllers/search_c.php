388e9f02ad69fb4c2c7cfcb46a2cc4c58bd59fb3
zhanghaidong <1826962986@qq.com>
2014-08-19 20:41:08 +0800

    update by haidong

--- application/controllers/search_c.php
+++ application/controllers/search_c.php
@@ -10,22 +10,26 @@ class Search_c extends MY_Controller {
     public function index()
     {
         $data = array();
+        $searchData = array();
 
         $user = $this->session->all_userdata();
         log_message('info', "search_c index user:".var_export($user,true));
         log_message('info', "search_c index post:".var_export($_POST,true));
 
+        $data['searchData'] = @json_encode(array('Rows'=>$searchData));
         $this->load->view('search_lst_v',$data);
     }
 
     public function search()
     {
         $data = array();
+        $searchData = array();
 
         $user = $this->session->all_userdata();
         log_message('info', "search_c index user:".var_export($user,true));
         log_message('info', "search_c index post:".var_export($_POST,true));
 
+        $data['searchData'] = @json_encode(array('Rows'=>$searchData));
         $this->load->view('search_lst_v',$data);
     }
 }
