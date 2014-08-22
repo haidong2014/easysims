b4ea1e4a573fa1e9015dd14fc28d6b95dab53be0
zhanghaidong <1826962986@qq.com>
2014-08-22 22:54:38 +0800

    update by haidong

--- application/models/subject_m.php
+++ application/models/subject_m.php
@@ -1,19 +1,20 @@
 <?php
-class Course_m extends MY_Model
+class Subject_m extends MY_Model
 {
 
     public function __construct()
     {
         parent::__construct();
         $this->CI->load->database();
-        $this->table_name='ss_course';
+        $this->table_name='ss_subject';
     }
 
-    public function getList($keyword)
+    public function getList($course_id, $keyword)
     {
-        $this->db->select('course_id,course_name,remarks');
+        $this->db->select('course_id,subject_id,subject_name,period,remarks');
+        $this->db->where( 'course_id', $course_id );
         if(!empty($keyword)){
-            $this->db->like('course_name',$keyword);
+            $this->db->like('subject_name',$keyword);
         }
         $this->db->where('delete_flg', 0);
         $query =  $this->db->get($this->table_name);
@@ -21,7 +22,9 @@ class Course_m extends MY_Model
     }
 
     public function addOne($data){
-        $this->db->set( 'course_name',	$data['course_name'] );
+        $this->db->set( 'course_id',	$data['course_id'] );
+        $this->db->set( 'subject_name',	$data['subject_name'] );
+        $this->db->set( 'period',	    $data['period'] );
         $this->db->set( 'remarks',		$data['remarks'] );
         $this->db->set( 'delete_flg',	$data['delete_flg'] );
         $this->db->set( 'insert_user',	$data['insert_user'] );
@@ -32,9 +35,10 @@ class Course_m extends MY_Model
         return $this->db->insert( $this->table_name );
     }
 
-    public function getOne($course_id){
-        $this->db->select('course_id,course_name,remarks');
+    public function getOne($course_id,$subject_id){
+        $this->db->select('course_id,subject_id,subject_name,period,remarks');
         $this->db->where('course_id', $course_id);
+        $this->db->where('subject_id', $subject_id);
         $this->db->where('delete_flg', 0);
         $query = $this->db->get($this->table_name);
         foreach ($query->result_array() as $row){
@@ -45,7 +49,9 @@ class Course_m extends MY_Model
 
     public function updateOne($data){
         $this->db->where('course_id',    $data['course_id']);
-        $this->db->set( 'course_name',	 $data['course_name']);
+        $this->db->where('subject_id',   $data['subject_id']);
+        $this->db->set( 'subject_name',	 $data['subject_name']);
+        $this->db->set( 'period',	     $data['period']);
         $this->db->set( 'remarks',		 $data['remarks']);
         $this->db->set( 'update_user',	 $data['update_user'] );
         $this->db->set( 'update_time',	 $data['update_time'] );
@@ -53,10 +59,11 @@ class Course_m extends MY_Model
     }
 
     public function deleteOne($data){
-        $this->db->where('course_id',   $data['course_id']);
-        $this->db->set( 'delete_flg',   $data['delete_flg']);
-        $this->db->set( 'update_user',	$data['update_user'] );
-        $this->db->set( 'update_time',	$data['update_time'] );
+        $this->db->where('course_id',    $data['course_id']);
+        $this->db->where('subject_id',   $data['subject_id']);
+        $this->db->set( 'delete_flg',    $data['delete_flg']);
+        $this->db->set( 'update_user',   $data['update_user'] );
+        $this->db->set( 'update_time',	 $data['update_time'] );
         return $this->db->update( $this->table_name );
     }
 }
