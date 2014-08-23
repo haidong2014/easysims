645b9137e5803706794dc20cb9b91f27ad607d3a
liweijp <liweijp@gmail.com>
2014-08-23 22:06:35 +0900

    li update

--- application/views/student_add_v.php
+++ application/views/student_add_v.php
@@ -105,11 +105,12 @@ if(!empty($student_id)){
             </tr>
              <tr>
                 <td align="right" class="l-table-edit-td">年龄:</td>
-                <td align="left" class="l-table-edit-td"><input name="age" type="text" id="age" ltype="text" value="<?php echo @$age ?>"  /></td>
+                <td align="left" class="l-table-edit-td">
+                 <input name="txtAge" type="text" id="txtAge" ltype='spinner' ligerui="{type:'int'}" value="<?php echo $age?>"/></td>
                 <td align="left"></td>
             </tr>
              <tr>
-                <td align="right" class="l-table-edit-td">卡号:</td>
+                <td align="right" class="l-table-edit-td">身份证号::</td>
                 <td align="left" class="l-table-edit-td"><input name="id_card" type="text" id="id_card" ltype="text" value="<?php echo @$id_card ?>"  /></td>
                 <td align="left"></td>
             </tr>
@@ -119,42 +120,178 @@ if(!empty($student_id)){
                 <td align="left"></td>
             </tr>
              <tr>
-                <td align="right" class="l-table-edit-td">父母电话:</td>
+                <td align="right" class="l-table-edit-td">家长电话:</td>
                 <td align="left" class="l-table-edit-td"><input name="parent_phone" type="text" id="parent_phone" ltype="text" value="<?php echo @$parent_phone ?>" /></td>
                 <td align="left"></td>
             </tr>
             <tr>
-                <td align="right" class="l-table-edit-td">课程(<input type="button" value="设定" onclick="setCourse()"/>):</td>
-                <td align="left" class="l-table-edit-td">
-					<select name="course_no" id="course_no" ltype="select">
-						<?php foreach($courseData as $course){?>
-    						<?php if(@$course_no==($course['course_no'])){ ?>
-    						<option value="<?php  echo $course['course_no'] ?>" selected><?php  echo $course['course_name'] ?></option>
-    						<?php }else{?>
-    						<option value="<?php  echo $course['course_no'] ?>"><?php  echo $course['course_name'] ?></option>
-    						<?php } ?>
-						<?php } ?>
+                <td align="right" class="l-table-edit-td">毕业学校:</td>
+                <td align="left" class="l-table-edit-td"><input name="txtGraduateSchool" type="text" id="txtGraduateSchool" ltype="text"/></td>
+                <td align="left"></td>
+            </tr>
+            <tr>
+                <td align="right" class="l-table-edit-td">毕业专业:</td>
+                <td align="left" class="l-table-edit-td"><input name="txtSpecialty" type="text" id="txtSpecialty" ltype="text"/></td>
+                <td align="left"></td>
+            </tr>
+            <tr>
+                <td align="right" class="l-table-edit-td">学历:</td>
+				<td align="left" class="l-table-edit-td">
+					<select name="ddlEducation" id="ddlEducation" ltype="select">
+						<option value="1">本科以上</option>
+						<option value="2">本科</option>
+						<option value="3">专科</option>
+						<option value="4">专科以下</option>
 					</select>
-					
+				</td>
+                <td align="left"></td>
+            </tr>
+            <tr>
+                <td align="right" class="l-table-edit-td">课程:</td>
+				<td align="left" class="l-table-edit-td">
+					<select name="ddlSpecialty" id="ddlSpecialty" ltype="select">
+						<option value="1">建筑动画课程信息</option>
+						<option value="2">室内设计课程信息</option>
+						<option value="3">影视动画课程信息</option>
+						<option value="4">影视后期课程信息</option>
+						<option value="5">影视坐标合作班课程信息</option>
+					</select>
+				</td>
+                <td align="left"></td>
+            </tr>
+            <tr>
+                <td align="right" class="l-table-edit-td">学费:</td>
+                <td align="left" class="l-table-edit-td">
+                    <input name="txtSchooling" type="text" id="txtSchooling" ltype='spinner' ligerui="{type:'int'}" />
+                </td><td align="left"></td>
+            </tr>
+            <tr>
+                <td align="right" class="l-table-edit-td">原籍</td>
+                <td align="left" class="l-table-edit-td"> 
+                <textarea cols="100" rows="4" class="l-textarea" id="ancestralhome" style="width:400px"></textarea>
+                </td><td align="left"></td>
+            </tr>
+            <tr>
+                <td align="right" class="l-table-edit-td">通过什么<br>方式了解<br>到学校:</td>
+                <td align="left" class="l-table-edit-td"> 
+                <textarea cols="100" rows="4" class="l-textarea" id="knowme" style="width:400px"></textarea>
+                </td><td align="left"></td>
+            </tr>
+            <tr>
+                <td align="right" class="l-table-edit-td">对行业的<br>了解情况:</td>
+                <td align="left" class="l-table-edit-td"> 
+                <textarea cols="100" rows="4" class="l-textarea" id="knowtrade" style="width:400px"></textarea>
+                </td><td align="left"></td>
+            </tr>
+            <tr>
+                <td align="right" class="l-table-edit-td">个人喜好:</td>
+                <td align="left" class="l-table-edit-td"> 
+                <textarea cols="100" rows="4" class="l-textarea" id="preference" style="width:400px"></textarea>
+                </td><td align="left"></td>
+            </tr>
+            <tr>
+                <td align="right" class="l-table-edit-td">有无软件<br>基础:</td>
+                <td align="left" class="l-table-edit-td"> 
+                <textarea cols="100" rows="4" class="l-textarea" id="software_base" name="software_base" style="width:400px" ><?php echo $software_base?></textarea>
+                </td><td align="left"></td>
+            </tr>
+            <tr>
+                <td align="right" class="l-table-edit-td">来校学习<br>的目的:</td>
+                <td align="left" class="l-table-edit-td"> 
+                <textarea cols="100" rows="4" class="l-textarea" id="purpose" name="purpose" style="width:400px" ><?php echo $purpose?></textarea>
+                </td><td align="left"></td>
+            </tr>
+            <tr>
+                <td align="right" class="l-table-edit-td">入学年度:</td>
+				<td align="left" class="l-table-edit-td">
+					<select name="ddlYear" id="ddlYear" ltype="select">
+						<option value="1">2014</option>
+						<option value="2">2013</option>
+						<option value="3">2012</option>
+						<option value="4">2011</option>
+						<option value="5">2010</option>
+						<option value="6">2009</option>
+						<option value="7">2008</option>
+						<option value="8">2007</option>
+						<option value="9">2006</option>
+						<option value="10">2005</option>
+					</select>
+				</td>
+                <td align="left"></td>
+            </tr>
+            <tr>
+                <td align="right" class="l-table-edit-td">入学月份:</td>
+				<td align="left" class="l-table-edit-td">
+					<select name="ddlMonth" id="ddlMonth" ltype="select">
+						<option value="1">1</option>
+						<option value="2">2</option>
+						<option value="3">3</option>
+						<option value="4">4</option>
+						<option value="5">5</option>
+						<option value="6">6</option>
+						<option value="7">7</option>
+						<option value="8">8</option>
+						<option value="9">9</option>
+						<option value="10">10</option>
+						<option value="11">11</option>
+						<option value="12">12</option>
+					</select>
+				</td>
+                <td align="left"></td>
+            </tr>
+            <tr>
+                <td align="right" class="l-table-edit-td">开课日期:</td>
+                <td align="left" class="l-table-edit-td">
+                    <input name="txtStartDate" type="text" id="txtStartDate" ltype="date" />
+                </td><td align="left"></td>
+            </tr>
+            <tr>
+                <td align="right" class="l-table-edit-td">结课日期:</td>
+                <td align="left" class="l-table-edit-td">
+                    <input name="txtEndDate" type="text" id="txtEndDate" ltype="date" />
+                </td><td align="left"></td>
+            </tr>
+            <tr>
+                <td align="right" class="l-table-edit-td">成绩:</td>
+                <td align="left" class="l-table-edit-td">
+                    <input name="txtScore" type="text" id="purpose" ltype='spinner'  ligerui="{type:'int'}" validate="{digits:true,min:1,max:100}" <?php echo $purpose;?>/>
+                </td><td align="left"></td>
+            </tr>
+            <tr>
+                <td align="right" class="l-table-edit-td">就业城市:</td>
+                <td align="left" class="l-table-edit-td"><input name="follow_city" type="text" id="follow_city" ltype="text" <?php echo $follow_city?>/></td>
+                <td align="left"></td>
+            </tr>
+            <tr>
+                <td align="right" class="l-table-edit-td">就业企业:</td>
+                <td align="left" class="l-table-edit-td"><input name="follow_company" type="text" id="follow_company" ltype="text" value="<?php echo $follow_company?>" /></td>
+                <td align="left"></td>
+            </tr>
+            <tr>
+                <td align="right" class="l-table-edit-td">就业薪资:</td>
+                <td align="left" class="l-table-edit-td">
+                    <input name="follow_salary" type="text" id="follow_salary" ltype='spinner' ligerui="{type:'int'}" value="<?php echo $follow_salary;?>"/>
+                </td><td align="left"></td>
+            </tr>
+            <tr>
+                <td align="right" class="l-table-edit-td">就业职位:</td>
+                <td align="left" class="l-table-edit-td"><input name="follow_position" type="text" id="follow_position" ltype="text" value="<?php echo $follow_position;?>" /></td>
+                <td align="left"></td>
+            </tr>
+            <tr>
+                <td align="right" class="l-table-edit-td">就业情况<br>备注:</td>
+                <td align="left" class="l-table-edit-td"> 
+                <textarea cols="100" rows="4" class="l-textarea" id="follow" style="width:400px"><?php echo $follow_remarks;?></textarea>
+                </td><td align="left"></td>
+            </tr>
+            <tr>
+                <td align="right" class="l-table-edit-td" valign="top">系统用户:</td>
+                <td align="left" class="l-table-edit-td">
+                     <input id="system_user_0" type="checkbox" name="system_user" value="1" <?php echo (intval(@$system_user)==1?"checked":"")?> /><label for="system_user_0">有效</label>
                 </td>
                 <td align="left"></td>
             </tr>
-             <tr>
-                <td align="right" class="l-table-edit-td">班级:</td>
-                <td align="left" class="l-table-edit-td">
-					<select name="class_no" id="class_no" ltype="select">
-						<?php foreach($classData as $class){?>
-    						<?php if(@$class_no==($class['class_no'])){ ?>
-    						<option value="<?php  echo $class['class_no'] ?>" selected><?php  echo $class['class_name'] ?></option>
-    						<?php }else{?>
-    						<option value="<?php  echo $class['class_no'] ?>"><?php  echo $class['class_name'] ?></option>
-    						<?php } ?>
-						<?php } ?>
-					</select>
-					
-                </td>
-                <td align="left"></td>
-            </tr>
+
             <tr>
                 <td align="right" class="l-table-edit-td">备注:</td>
                 <td align="left" class="l-table-edit-td">
