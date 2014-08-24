6bc367c4c3519c37662eacabad020876b3e34dbe
zhanghaidong <1826962986@qq.com>
2014-08-24 21:17:56 +0800

    update by haidong

--- application/controllers/student_c.php
+++ application/controllers/student_c.php
@@ -1,61 +1,64 @@
-<?php 
+<?php
 class Student_c extends MY_Controller {
-  public function __construct()
-   {
+
+    public function __construct()
+    {
         parent::__construct();
         $this->load->model('student_m','student_m');
-   }
+    }
 
-	public function index()
-	{
-      $data = array();
-      $user = $this->session->all_userdata();
-      log_message('info', "####user".var_export($user,true));
-      $now = date('Ymd');
-      $keyword = $this->input->post('txtKey');
-      $start_year = $this->input->post('start_year');
-      $start_month = $this->input->post('start_month');
-      //echo "ggg:".$start_year."|".$start_month;
-      $studentData = $this->student_m->getList($keyword, $start_year, $start_month);
-      foreach($studentData as &$data){
-        $data['opt']="<a href=\"".SITE_URL."/student_c/view_student_init/".$data['student_id']."\">查看</a> |".
-        "<a href=\"".SITE_URL."/student_c/upd_student_init/".$data['student_id']."\">编辑</a> |".
-        "<a href=\"#\" onclick=\"delstudent('".SITE_URL."/student_c/delete_student/".$data['student_id']."')\">删除</a>";
-        
-        //$data['age'] = floor(($now-str_replace("-","",$data['birthday']))/10000);
-        log_message('info', "####href".$data['opt']);
-      }
-      $data['studentData'] = $studentData;
-      $data['studentsData'] = @json_encode(array('Rows'=>$studentData));
-      $this->load->model('code_m','code_m');
-      $graduateData = $this->code_m->getList("06");
-      $data['graduateData'] = $graduateData;
-      $data['start_year'] = $start_year;
-      $data['start_month'] = $start_month;
-      $data['txtKey'] = $keyword;
-      $this->load->view('student_lst_v',$data);
-	}
-	public function add_student_init(){
-	  $data = array();
-	   $this->load->model('code_m','code_m');
-      $graduateData = $this->code_m->getList("06");
-      $data['graduateData'] = $graduateData;
-      $this->load->model('course_m','course_m');
-      $courseData = $this->course_m->getList(null);
-        
+    public function index()
+    {
+        $data = array();
+        $keyword = $this->input->post('txtKey');
+        $start_year = $this->input->post('start_year');
+        if (empty($start_year)) {
+            $start_year = substr(date("Y-m-d"),0,4);;
+        } else {
+            $start_year = trim($start_year);
+        }
+        $start_month = $this->input->post('start_month');
+        if (empty($start_month)) {
+            $start_month = substr(date("Y-m-d"),5,2);
+        } else {
+            $start_month = substr("0".trim($start_month),-2);
+        }
+        log_message('info', "Student_c index start_month".$start_month);
+        $studentData = $this->student_m->getList($keyword, $start_year, $start_month);
+        foreach($studentData as &$data){
+            $data['opt']="<a href=\"".SITE_URL."/student_c/view_student_init/".$data['student_id']."\">查看</a> |".
+            "<a href=\"".SITE_URL."/student_c/upd_student_init/".$data['student_id']."\">编辑</a> |".
+            "<a href=\"#\" onclick=\"delstudent('".SITE_URL."/student_c/delete_student/".$data['student_id']."')\">删除</a>";
+        }
+        $data['start_year'] = $start_year;
+        $data['start_month'] = $start_month;
+        $data['txtKey'] = $keyword;
+        $data['studentsData'] = @json_encode(array('Rows'=>$studentData));
+                log_message('info', "Student_c index data".var_export($data,true));
+        $this->load->view('student_lst_v',$data);
+    }
+
+    public function add_student_init(){
+        $data = array();
+        $this->load->model('code_m','code_m');
+        $graduateData = $this->code_m->getList("06");
+        $data['graduateData'] = $graduateData;
+        $this->load->model('course_m','course_m');
+        $courseData = $this->course_m->getList(null);
         $data['courseData'] = $courseData;
-	  $this->load->view('student_add_v',$data);
-	}
-	public function add_student(){
-	   log_message('info','add student '.var_export($_POST,true));
-	   $userinfo = $this->session->userdata('user');
-	   $student_no = $this->input->post('student_no');
-	   $student_name = $this->input->post('student_name');
-	   $sex = $this->input->post('sex');
-	   $birthday = $this->input->post('birthday');
-	   $age = $this->input->post('age');
-	   $id_card = $this->input->post('id_card');
-	   $contact_way = $this->input->post('contact_way');
+        $this->load->view('student_add_v',$data);
+    }
+
+  public function add_student(){
+     log_message('info','add student '.var_export($_POST,true));
+     $userinfo = $this->session->userdata('user');
+     $student_no = $this->input->post('student_no');
+     $student_name = $this->input->post('student_name');
+     $sex = $this->input->post('sex');
+     $birthday = $this->input->post('birthday');
+     $age = $this->input->post('age');
+     $id_card = $this->input->post('id_card');
+     $contact_way = $this->input->post('contact_way');
        $parent_phone = $this->input->post('parent_phone');
        $course_no = $this->input->post('course_no');
        $class_no = $this->input->post('class_no');
@@ -69,11 +72,11 @@ class Student_c extends MY_Controller {
        $remarks = $this->input->post('remarks');
       /*************other***************/
        $graduate_school = $this->input->post('graduate_school');
-	   $specialty = $this->input->post('specialty');
-	   $graduate = $this->input->post('graduate');
-	   $ancestralhome = $this->input->post('ancestralhome');
-	   $know_school = $this->input->post('know_school');
-	   $know_trade = $this->input->post('know_trade');
+     $specialty = $this->input->post('specialty');
+     $graduate = $this->input->post('graduate');
+     $ancestralhome = $this->input->post('ancestralhome');
+     $know_school = $this->input->post('know_school');
+     $know_trade = $this->input->post('know_trade');
        $preference = $this->input->post('preference');
        $software_base = $this->input->post('software_base');
        $purpose = $this->input->post('purpose');
@@ -82,60 +85,60 @@ class Student_c extends MY_Controller {
        $follow_salary = $this->input->post('follow_salary');
        $follow_position = $this->input->post('follow_position');
        $follow_remarks = $this->input->post('follow_remarks');
-       
+
        /********************************************/
-	   $student_id = $this->student_m->addOne($student_no,$student_name, $sex,$birthday, $age, $id_card, $contact_way, 
+     $student_id = $this->student_m->addOne($student_no,$student_name, $sex,$birthday, $age, $id_card, $contact_way,
        $parent_phone, $course_no, $class_no, $cost, $start_year,$start_month,$start_date,$end_date,
        $attendance,$system_user,$remarks,$userinfo);
          /***insert other**/
          $this->student_m->addOneOther($student_id, $student_no, $graduate_school, $specialty,$graduate
         ,$ancestralhome,$know_school,$know_trade,$preference,$software_base,$purpose,
         $follow_city,$follow_company,$follow_salary,$follow_position,$follow_remarks,$userinfo);
-        
-        
+
+
        if($system_user == "1"){
             self::addUser($student_no,$student_name);
         }
         redirect("student_c");
-	  
-	}
-	public function upd_student_init($student_id = null){
-	  
-	  $data = array();
-	  if(empty($student_id)){
+
+  }
+  public function upd_student_init($student_id = null){
+
+    $data = array();
+    if(empty($student_id)){
         $student_id = $this->input->get('student_id');
         if(empty($student_id)){
           $student_id = $this->input->post('student_id');
         }
      }
-	 
-	  $studentData = $this->student_m->getOne($student_id);
 
-	  $otherData = $this->student_m->getOneOther($student_id);
-	  $data = $studentData+$otherData;
-	  $this->load->model('code_m','code_m');
+    $studentData = $this->student_m->getOne($student_id);
+
+    $otherData = $this->student_m->getOneOther($student_id);
+    $data = $studentData+$otherData;
+    $this->load->model('code_m','code_m');
       $graduateData = $this->code_m->getList("06");
       $data['graduateData'] = $graduateData;
-      
+
       $this->load->model('course_m','course_m');
       $courseData = $this->course_m->getList(null);
-        
+
         $data['courseData'] = $courseData;
-        
-	  $this->load->view('student_add_v', $data);
-	}
+
+    $this->load->view('student_add_v', $data);
+  }
     public function upd_student($student_id = null){
       if(empty($student_id)){
         $student_id = $this->input->post('student_id');
       }
        $student_id = $this->input->post('student_id');
        $student_no = $this->input->post('student_no');
-	   $student_name = $this->input->post('student_name');
-	   $sex = $this->input->post('sex');
-	   $birthday = $this->input->post('birthday');
-	   $age = $this->input->post('age');
-	   $id_card = $this->input->post('id_card');
-	   $contact_way = $this->input->post('contact_way');
+     $student_name = $this->input->post('student_name');
+     $sex = $this->input->post('sex');
+     $birthday = $this->input->post('birthday');
+     $age = $this->input->post('age');
+     $id_card = $this->input->post('id_card');
+     $contact_way = $this->input->post('contact_way');
        $parent_phone = $this->input->post('parent_phone');
        $course_no = $this->input->post('course_no');
        $class_no = $this->input->post('class_no');
@@ -149,11 +152,11 @@ class Student_c extends MY_Controller {
        $remarks = $this->input->post('remarks');
           /*************other***************/
        $graduate_school = $this->input->post('graduate_school');
-	   $specialty = $this->input->post('specialty');
-	   $graduate = $this->input->post('graduate');
-	   $ancestralhome = $this->input->post('ancestralhome');
-	   $know_school = $this->input->post('know_school');
-	   $know_trade = $this->input->post('know_trade');
+     $specialty = $this->input->post('specialty');
+     $graduate = $this->input->post('graduate');
+     $ancestralhome = $this->input->post('ancestralhome');
+     $know_school = $this->input->post('know_school');
+     $know_trade = $this->input->post('know_trade');
        $preference = $this->input->post('preference');
        $software_base = $this->input->post('software_base');
        $purpose = $this->input->post('purpose');
@@ -164,7 +167,7 @@ class Student_c extends MY_Controller {
        $follow_remarks = $this->input->post('follow_remarks');
        $userinfo = $this->session->userdata('user');
        /********************************************/
-      $this->student_m->updateOne($student_no,$student_name, $sex,$birthday, $age, $id_card, $contact_way, 
+      $this->student_m->updateOne($student_no,$student_name, $sex,$birthday, $age, $id_card, $contact_way,
          $parent_phone, $course_no, $class_no, $cost, $start_year,$start_month,$start_date,$end_date,
          $attendance,$system_user,$remarks,$student_id,$userinfo);
          /********************************************/
@@ -172,49 +175,49 @@ class Student_c extends MY_Controller {
     ,$ancestralhome,$know_school,$know_trade,$preference,$software_base,$purpose,
     $follow_city,$follow_company,$follow_salary,$follow_position,$follow_remarks,$userinfo);
          /********************************************/
-         
-         
+
+
          if($system_user == "1"){
             self::addUser($student_no,$student_name);
         }
       redirect("student_c");
-	}
+  }
     public function view_student_init($student_id = null){
-	  
-	  $data = array();
-	  if(empty($student_id)){
+
+    $data = array();
+    if(empty($student_id)){
         $student_id = $this->input->get('student_id');
         if(empty($student_id)){
           $student_id = $this->input->post('student_id');
         }
      }
-	 
-	  $studentData = $this->student_m->getOne($student_id);
-	  $this->load->model('course_m','course_m');
-	  //var_dump($studentData['course_no']);
-	  $courseData = $this->course_m->getOneByNo($studentData['course_no']);
-	  //var_dump($courseData);
-	  $studentData['course_name'] = $courseData['course_name'];
+
+    $studentData = $this->student_m->getOne($student_id);
+    $this->load->model('course_m','course_m');
+    //var_dump($studentData['course_no']);
+    $courseData = $this->course_m->getOneByNo($studentData['course_no']);
+    //var_dump($courseData);
+    $studentData['course_name'] = $courseData['course_name'];
       $otherData = $this->student_m->getOneOther($student_id);
-      
+
       $this->load->model('code_m','code_m');
       $graduateData = $this->code_m->getList("06");
       //var_dump($graduateData);
       $studentData['graduate'] = $graduateData['GRADUATE'][$otherData['graduate']];
-      
-      
-	  $this->load->view('student_view_v',$studentData+$otherData);
-	}
+
+
+    $this->load->view('student_view_v',$studentData+$otherData);
+  }
    public function delete_student($student_id = null){
       if(empty($student_id)){
         $student_id = $this->input->post('student_id');
       }
-      
+
       $this->student_m->deleteOne($student_id);
       $this->student_m->deleteOneOther($student_id);
       redirect("student_c");
-	}
-	
+  }
+
     public function chk_repeat($student_no, $old_no = null){
         log_message('info', "student_c chk_repeat start");
         log_message('info', "student_c chk_repeat post user:".$student_no." : :".$old_no);
@@ -227,7 +230,7 @@ class Student_c extends MY_Controller {
 
         log_message('info', "student chk_repeat end".var_export($checkuser,true));
     }
-    
+
     private function addUser($teacher_no,$teacher_name){
         $userinfo = $this->session->userdata('user');
 
