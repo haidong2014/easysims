2ed218d3029a88d2f45a747c10af056a1e7c8b4b
zhanghaidong <1826962986@qq.com>
2014-09-17 20:22:58 +0800

    update by haidong

--- application/config/database.php
+++ application/config/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'mysql316.db.sakura.ne.jp';
-$db['default']['username'] = 'shengyi';
-$db['default']['password'] = 'ns52rettg4';
-$db['default']['database'] = 'shengyi_easyss';
+$db['default']['hostname'] = '127.0.0.1';
+$db['default']['username'] = 'root';
+$db['default']['password'] = '';
+$db['default']['database'] = 'easyss';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
